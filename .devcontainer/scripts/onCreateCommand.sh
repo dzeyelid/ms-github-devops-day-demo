634773828adcc3a0b74bf8564769c03921ce1d0d
Kazumi IWANAGA (OHIRA) / @dz_ <dzeyelid@gmail.com>
2022-08-30 08:19:26 +0900

    dev container の修正 (#10)

--- .devcontainer/scripts/onCreateCommand.sh
+++ .devcontainer/scripts/onCreateCommand.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 
-node -v
-if [ 0 -lt $? ]; then
-nvm install 16
-fi
-
-npm install -g npm
-npm install -g @azure/static-web-apps-cli
-
 az bicep version
 if [ 0 -lt $? ]; then
   az bicep install
