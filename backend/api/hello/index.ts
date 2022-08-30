610d27d09ad3ff91e6f3a50d4eb56c86016db40e
Kazumi IWANAGA (OHIRA) / @dz_ <dzeyelid@gmail.com>
2022-08-30 01:06:33 +0000

    Update function

--- backend/api/hello/index.ts
+++ backend/api/hello/index.ts
@@ -2,14 +2,20 @@ import { AzureFunction, Context, HttpRequest } from "@azure/functions"
 
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
   context.log('HTTP trigger function processed a request.');
+
   const name = (req.query.name || (req.body && req.body.name));
-  const responseMessage = name
-    ? `Hello, ${name}!`
-    : 'Hoooray!'
+  const message = name ? `Hello, ${name}!` : 'Hoooray!'
+
+  const headers = {
+    'content-type': 'application/json',
+  }
 
   context.res = {
     // status: 200, /* Defaults to 200 */
-    body: responseMessage
+    headers,
+    body: {
+      message
+    }
   };
 
 };
